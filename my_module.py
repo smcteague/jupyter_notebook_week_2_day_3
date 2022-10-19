552ddd1793f0721f1e973da9fba55bc60356d0a2
Steve McTeague <smcteague@yahoo.com>
2022-10-19 18:21:44 -0500

    initial commit

---
+++ my_module.py
@@ -0,0 +1,14 @@
+def squareFootageOfHouse(length, width):
+    """
+    Returns the square footage of a house. 
+    Inputs are: length and width.
+    """
+    return length * width
+
+def circumferenceOfCircle(radius):
+    from math import pi
+    """
+    Returns the circumference a circle.
+    Input is: radius.
+    """
+    return (2 * pi) * radius
